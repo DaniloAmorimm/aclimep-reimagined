04c71b0a98dd394e75e3f1ed0a3f36856e08cdcf
DaniloAmorimm <dan.amorim.bf@gmail.com>
2025-11-10 10:09:14 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// ✅ IMPORTANTE: importe o BrowserRouter
+import { BrowserRouter } from "react-router-dom";
+
+// createRoot(document.getElementById("root")!).render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* ✅ Defina o basename para GitHub Pages */}
+    <BrowserRouter basename="/aclimep-reimagined/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
