dd4ea0b47bb54c676ad0b7cd42b790fc1e82fe76
DaniloAmorimm <dan.amorim.bf@gmail.com>
2025-11-10 10:48:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 
 // ✅ IMPORTANTE: importe o BrowserRouter
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // createRoot(document.getElementById("root")!).render(<App />);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* ✅ Defina o basename para GitHub Pages */}
-    <HashRouter basename="/aclimep-reimagined/">
+    <BrowserRouter basename="/aclimep-reimagined/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
