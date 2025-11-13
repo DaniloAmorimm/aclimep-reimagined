0c6c95191ac403926df228dc7254b2e0bc85ec53
Danilo Amorim <dan.amorim.bf@gmail.com>
2025-11-13 20:33:24 -0300

    implementação tempo open-meteo

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -32,7 +32,7 @@ const WeatherWidget = () => {
   useEffect(() => {
     const fetchLocation = async () => {
       try {
-        const res = await fetch("https://ipapi.co/json/");
+        const res = await fetch("https://ipwho.is/");
         const data = await res.json();
 
         if (data.success) {
